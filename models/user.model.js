6777422661da9bbba6eb0fc91daf54d08245f962
XinYan <vlxy020304@gmail.com>
2025-05-31 14:20:55 +0800

    feat(models): add initial User schema

---
+++ models/user.model.js
@@ -0,0 +1,106 @@
+const mongoose = require('mongoose');
+
+const friendSchema = new mongoose.Schema(
+    {
+        friendUid: {
+            type: String,
+            required: true
+        },
+
+        status: {
+            type: String,
+            enum: ["friends", "equest_sent", "request_received"],
+            required: true
+        },
+
+        requestTime: {
+            type: Date,
+            required: true
+        },
+    }, 
+    {
+        _id: false
+    }
+);
+
+const stripeAccountSchema = new mongoose.Schema(
+    {
+        stripeAccountID: {
+            type: String, 
+            required: true
+        },
+        
+        detailsSubmitted: {
+            type: Boolean,
+            required: true,
+            default: false
+        },
+
+        chargesEnabled: {
+            type: Boolean,
+            required: true,
+            default: false
+        },
+
+        payoutsEnabled: {
+            type: Boolean,
+            required: true,
+            default: false
+        }
+    },
+    {
+        _id: false
+    }
+);
+
+const userSchema = new mongoose.Schema(
+    {
+        uid:{
+            type: String,
+            required: true,
+            unique: true
+        },
+
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            minlength: 3,
+            maxlength: 20,
+            match: /^(?!_+$)[a-zA-Z0-9_]+$/
+        },
+
+        profilePicUrl: {
+            type: String,
+            required: false
+        },
+
+        notificationEnabled: {
+            type: Boolean,
+            default: true
+        },
+
+        theme: {
+            type: String,
+            enum: ["light", "dark", "system"],
+            default: "system"
+        },
+
+        friends: {
+            type: [friendSchema],
+            default: []
+        },
+
+        stripeAccount: {
+            type: stripeAccountSchema,
+            required: false
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
