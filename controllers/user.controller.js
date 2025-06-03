07b17711a197d7b2fdfdd135246cce73d9c5895b
XinYan <vlxy020304@gmail.com>
2025-06-03 12:33:35 +0800

    feat(auth): modify signup api to sync api, test api using Postman
    - Refactor signUp API into a single syncUser API to handle both sign-up and sign-in flows to prevent duplicate users in database while using Firebase Auth for authentication
    - Return user onboarding status to frontend to direct users appropriately (onboarding or home screen)
    - Fix bug handling undefined profilePicUrl when updating user profile
    - Tested both APIs with Postman, bypassing Firebase Auth middleware for now

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,13 +1,19 @@
-const {validateUsername} = require("../validators/username.validator.js");
-const {isUsernameTaken, updateUserProfile} = require("../services/user.service.js");
-const User = require("../models/user.model.js");
+const validateUsername = require("../validators/username.validator.js");
+const {signUpOrSignIn, isUsernameTaken, updateUserProfile} = require("../services/user.service.js");
 
-const signUpUser = async (req, res) => {
+const syncUser = async (req, res) => {
     const uid = req.user.uid;
+    // for testing without middleware: const {uid} = req.body; 
 
     try {
-        const user = await User.create({uid});
-        return res.status(201).json(user);
+        const response = await signUpOrSignIn(uid);
+        if (!response.existed) { // new user
+            return res.status(201).json({onboard: "incomplete", data: response.user})
+        } else if (!response.onboard) { // existing user with no username, need direct to onboarding
+            return res.status(200).json({onboard: "incomplete", data: response.user})
+        } else { // existing user with username, direct to home screen
+        return res.status(200).json({onboard: "complete", data: response.user});
+        }
     } catch (error) {
         return res.status(500).json({message: error.message});
     }
@@ -16,6 +22,7 @@ const signUpUser = async (req, res) => {
 const updateUserProfileController = async (req, res) => {
     const uid = req.user.uid;
     const {username, profilePicUrl} = req.body;
+    // for testung without middleware: const {uid, username, profilePicUrl} = req.body;
 
     const validationError = validateUsername(username);
     if (validationError) {
@@ -39,6 +46,6 @@ const updateUserProfileController = async (req, res) => {
 }
 
 module.exports = {
-    signUpUser,
+    syncUser,
     updateUserProfileController
 };
--- services/user.service.js
+++ services/user.service.js
@@ -1,20 +1,42 @@
 const User = require("../models/user.model.js");
 
+const signUpOrSignIn = async (uid) => {
+    const existingUser = await User.findOne({uid}); //key and variable names are the same
+    if (!existingUser) {
+        const newUser = await User.create({uid});
+        return {existed: false, user: newUser};
+    } else if (!existingUser.username) {
+        return {existed: true, onboard: false, user: existingUser}
+    } else {
+        return {existed: true, onboard: true, user: existingUser}
+    }
+}
+
 const isUsernameTaken = async (username) => {
     const existingUser = await User.findOne({username}); //key and variable names are the same 
     return !!existingUser;
 }
 
 const updateUserProfile = async (uid, username, profilePicUrl) => {
-    const updatedProfile = await User.findOneAndUpdate(
-        {uid}, //key and variable names are the same 
-        {$set: {username, profilePicUrl}}, //key and variable names are the same 
-        {new: true, runValidators: true}
-    )
-    return updatedProfile;
+    if (!profilePicUrl) {
+        const updatedProfile = await User.findOneAndUpdate(
+            {uid}, //key and variable names are the same 
+            {$set: {username}}, //key and variable names are the same 
+            {new: true, runValidators: true}
+        )
+        return updatedProfile;
+    } else {
+        const updatedProfile = await User.findOneAndUpdate(
+            {uid}, //key and variable names are the same 
+            {$set: {username, profilePicUrl}}, //key and variable names are the same 
+            {new: true, runValidators: true}
+        )
+        return updatedProfile
+    }
 }
 
 module.exports = {
+    signUpOrSignIn,
     isUsernameTaken, 
     updateUserProfile
 };
