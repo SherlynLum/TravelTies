3db990ac196d493a3cd10e030cc9752270a058db
XinYan <vlxy020304@gmail.com>
2025-06-27 10:26:24 +0800

    Add start script for Render deployment

--- TravelTies-frontend/app/(screens)/(tabs)/tripsDashboard.tsx
+++ TravelTies-frontend/app/(screens)/(tabs)/tripsDashboard.tsx
@@ -28,6 +28,7 @@ const Home = () => {
         return "Failed to load";
     }
   }
+/* for testing purpose: 
   useEffect(() => {
     const activeTrips = [{_id: "abc", name: "Japan trip", noOfParticipants: 1}, 
     {_id: "efg", name: "Korea trip", startDate: "2025-09-18", endDate: "2025-09-23", 
@@ -36,8 +37,7 @@ const Home = () => {
     
     setTrips(activeTrips)
   }, []);
-
-/*
+*/
   useEffect(() => {
     const getTrips = async () => {
       try {
@@ -69,7 +69,7 @@ const Home = () => {
     }
     getTrips();
   }, [])
-*/
+  
   return (
     <SafeAreaView className="flex-1 bg-white">
       <View className="flex flex-col px-5 pt-1 pb-2 gap-5">
