aa03650d20a63de871e7fd66378a051daae238ea
SherlynLum <lxinyu28905@gmail.com>
2025-07-13 00:21:05 +0800

    Update expense.model.js

--- TravelTies-backend/models/expense.model.js
+++ TravelTies-backend/models/expense.model.js
@@ -1,68 +1,56 @@
 const mongoose = require('mongoose');
 
 const owedBySchema = new mongoose.Schema({
-    owedByUid: {
-        type: String,
-        required: true
-    },
-
-    amount: {
-        type: Number,
-        required: true
-    },
-
-    isPaid: {
-        type: Boolean,
-        default: false
-    },
-
-    splitMethod: {
-        type: String,
-        enum: ['even', 'custom']
-},
-
+  owedByUid: {
+    type: String,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  isPaid: {
+    type: Boolean,
+    default: false
+  }
 }, {
-    _id: false
-})
+  _id: false
+});
 
 const expenseSchema = new mongoose.Schema({
-    expenseTrackerId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "ExpenseTracker",
-        required: true
-    },
-
-    name: {
-        type: String,
-        required: true
-    },
-
-    category: {
-        type: String,
-        enum: ["transportation", "accommodation", "food_and_drink", "entertainment", "shopping", "health"]
-    },
-
-    isShared: {
-        type: Boolean,
-        required: true
-    },
-
-    amountForPayer: {
-        // For individual expenses, it's the full amount they paid
-        // For shared expenses split evenly, it's the total divided by the number of people sharing 
-        // For shared expenses with custom splits, it's the specific amount the payer assigns to themselves
-        type: Number,
-        required: true
-    },
-
-    owedBy: {
-        type: [owedBySchema],
-        default: []
-    }
+  expenseTrackerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "ExpenseTracker",
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    enum: ["transportation", "accommodation", "food_and_drink", "entertainment", "shopping", "health"]
+  },
+  isShared: {
+    type: Boolean,
+    required: true
+  },
+  amountForPayer: {
+    type: Number,
+    required: true
+  },
+  splitMethod: {
+    type: String,
+    enum: ['even', 'custom'],
+    required: function () { return this.isShared; } // only required if shared
+  },
+  owedBy: {
+    type: [owedBySchema],
+    default: []
+  }
 }, {
-    timestamps: true
-})
+  timestamps: true
+});
 
 const Expense = mongoose.model("Expense", expenseSchema);
-
 module.exports = Expense;
