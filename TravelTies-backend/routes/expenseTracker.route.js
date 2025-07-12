815d39f2fc5e467f7fc4b05667284a12e4c026ce
SherlynLum <lxinyu28905@gmail.com>
2025-07-12 18:10:08 +0800

    Update expenseTracker.route.js

--- TravelTies-backend/routes/expenseTracker.route.js
+++ TravelTies-backend/routes/expenseTracker.route.js
@@ -8,4 +8,6 @@ router.post("/", expenseTrackerController.createExpenseTracker);
 // GET /api/expense-tracker/:tripId
 router.get("/:tripId", expenseTrackerController.getExpenseTracker);
 
+router.patch("/:tripId", expenseTrackerController.updateExpenseTracker);
+
 module.exports = router;
